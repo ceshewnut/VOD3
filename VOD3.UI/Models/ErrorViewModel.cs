398c0d256f9b138f14477a82918072314384ec1b
Jonas <info@csharpschool.com>
2019-09-27 09:59:43 +0200

    Add project files.

---
+++ VOD3.UI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VOD3.UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
